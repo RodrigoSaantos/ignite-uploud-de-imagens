bf9a08aa6e1630f0734929bc34e88650b8a032a3
Rodrigo Santos <contato.rodrigosaantos@gmail.com>
2021-06-05 11:49:20 -0300

    Challenge finished

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,17 +19,36 @@ export default function Home(): JSX.Element {
   } = useInfiniteQuery(
     'images',
     // TODO AXIOS REQUEST WITH PARAM
-    ,
+    async ({ pageParam = null }) => {
+      const response = await api.get('/api/images', {
+        params: {
+          after: pageParam,
+        },
+      });
+
+      return response.data;
+    },
     // TODO GET AND RETURN NEXT PAGE PARAM
+    {
+      getNextPageParam: lastPage => {
+        if (lastPage) {
+          return lastPage.after;
+        }
+        return null;
+      },
+    }
   );
 
   const formattedData = useMemo(() => {
     // TODO FORMAT AND FLAT DATA ARRAY
+    return data?.pages?.flatMap(page => page.data);
   }, [data]);
 
   // TODO RENDER LOADING SCREEN
+  if (isLoading) return <Loading />;
 
   // TODO RENDER ERROR SCREEN
+  if (isError) return <Error />;
 
   return (
     <>
@@ -38,6 +57,16 @@ export default function Home(): JSX.Element {
       <Box maxW={1120} px={20} mx="auto" my={20}>
         <CardList cards={formattedData} />
         {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
+        {hasNextPage && (
+          <Button
+            isLoading={isFetchingNextPage}
+            marginTop="8"
+            onClick={() => fetchNextPage()}
+            disabled={!hasNextPage}
+          >
+            Carregar mais
+          </Button>
+        )}
       </Box>
     </>
   );
